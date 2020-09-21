72fd897a10153e69243d93ec1179e4807df447ef
Leonid <kuznetsov_l@inbox.ru>
2020-09-21 18:12:16 +0300

    added solution

--- classify double/solution.c
+++ classify double/solution.c
@@ -15,8 +15,20 @@ uint64_t convertToUint64 (double number) {
 }
 
 bool getBit (const uint64_t number, const uint8_t index) {
-    /// Your code here...
+	uint64_t mask = 1ull << index;
+	uint64_t temp = mask & number;
+	temp = temp >> index;
+	return temp;
 }
+
+/**
+	* my Addigional Checkers prototypes:
+	**/
+
+bool checkSign(uint64_t number);
+bool checkInfinity(uint64_t number);
+bool checkNormal(uint64_t number);
+bool checkDenormal(uint64_t number) ;
 
 
 /**
@@ -25,6 +37,7 @@ bool getBit (const uint64_t number, const uint8_t index) {
 
 bool checkForPlusZero (uint64_t number) {
     /// Your code here.
+	return number == 0x0000000000000000;
 }
 
 bool checkForMinusZero (uint64_t number) {
@@ -32,35 +45,47 @@ bool checkForMinusZero (uint64_t number) {
 }
 
 bool checkForPlusInf (uint64_t number) {
-    /// Your code here.
+	return  checkSign(number) && checkInfinity(number);
 }
 
 bool checkForMinusInf (uint64_t number) {
-    /// Your code here.
+	return (~checkSign(number)) && checkInfinity(number);
 }
 
 bool checkForPlusNormal (uint64_t number) {
-    /// Your code here.
+	return checkSign(number) && checkNormal(number);
 }
 
 bool checkForMinusNormal (uint64_t number) {
-    /// Your code here.
+	return (~checkSign(number)) && checkNormal(number);
 }
 
 bool checkForPlusDenormal (uint64_t number) {
-    /// Your code here.
+	return checkSign(number) && checkDenormal(number);
 }
 
 bool checkForMinusDenormal (uint64_t number) {
-    /// Your code here.
+	return (~checkSign(number)) && checkDenormal(number);
 }
 
 bool checkForSignalingNan (uint64_t number) {
-    /// Your code here.
+	for (uint8_t i = 52ull; i <= 62ull; ++i)
+		{ if (getBit(number,i) == 0) return false; }
+			
+	bool has_one_flg = false;
+	for (uint8_t i = 0ull; i<51ull; ++i)
+		{if (getBit(number,i) == 1 ) has_one_flg = true; }
+	
+	if ((getBit(number,51ull) == 0 ) && (has_one_flg) ) return true;
+	else return false;
 }
 
 bool checkForQuietNan (uint64_t number) {
-    /// Your code here.
+	for (uint8_t i = 52ull; i <= 62ull; ++i)
+		{ if (getBit(number,i) == 0) return false; }
+	
+	if (getBit(number,51ull) == 1 ) return true;
+	else return false;
 }
 
 
@@ -108,4 +133,53 @@ void classify (double number) {
     else {
         printf("Error.\n");
     }
+}
+
+/* my Addigional Checkers  */
+
+bool checkSign(uint64_t number){
+	return getBit(number,63ull) == false;
+}
+
+bool checkInfinity(uint64_t number){
+	//exponent checking
+	for (uint8_t i = 52ull; i <= 62ull; ++i){ 
+		if (getBit(number,i) == 0 ) return false;
+		};
+	//fraction checking
+	for (uint8_t i = 0ull; i < 52ull; ++i ){
+		if (getBit(number,i) == 1 ) return false;
+		}
+	return true;
+}
+
+bool checkNormal(uint64_t number) {
+	//exponent checking 
+	bool has_one_flg = false;
+	bool has_zero_flg = false;
+	for (uint8_t i = 52ull; i <= 62ull; ++i){ 
+		  if (getBit(number,i) == 1){
+			has_one_flg = true;
+			}
+		  if (getBit(number,i) == 0){
+			has_zero_flg= true;
+		  }
+		  if (has_one_flg && has_zero_flg){
+			return true;
+		  }
+		}
+	return (has_one_flg && has_zero_flg);
+}
+
+bool checkDenormal(uint64_t number) {
+	//exponent 
+	for (uint8_t i = 52ull; i <= 62ull; ++i){ 
+		if (getBit(number,i) == 1) return false;
+		}
+	//fraction
+	bool has_one_flg = false;
+	for (uint8_t i = 0ull; i < 52ull; ++i ){
+		if (getBit(number,i) == 1 ) has_one_flg = true;
+		}		
+	return has_one_flg;
 }
