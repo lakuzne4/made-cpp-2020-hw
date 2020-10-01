5161afb17e7a2ddba0f7e74b6cacaf528022c00f
Leonid <kuznetsov_l@inbox.ru>
2020-10-01 17:37:45 +0300

    changed r5 to r3

--- count_det/src/main.s
+++ count_det/src/main.s
@@ -2,57 +2,57 @@
 .global count_det
 
 count_det:
-        mov r5, r0
+        mov r3, r0
 
-        ldr r0, [r5]
-        ldr r1, [r5, #16]
+        ldr r0, [r3]
+        ldr r1, [r3, #16]
         mul r1, r0
-        ldr r0, [r5, #32]
+        ldr r0, [r3, #32]
         mul r1, r0
 
         mov r2, r1
 //1
 
-        ldr r0, [r5, #4]
-        ldr r1, [r5, #20]
+        ldr r0, [r3, #4]
+        ldr r1, [r3, #20]
         mul r1, r0
-        ldr r0, [r5, #24]
+        ldr r0, [r3, #24]
         mul r1, r0
 
         add r2, r1
 //2
 
-        ldr r0, [r5, #8]
-        ldr r1, [r5, #12]
+        ldr r0, [r3, #8]
+        ldr r1, [r3, #12]
         mul r1, r0
-        ldr r0, [r5, #28]
+        ldr r0, [r3, #28]
         mul r1, r0
 
         add r2, r1
 //3
 
-        ldr r0, [r5]
-        ldr r1, [r5, #20]
+        ldr r0, [r3]
+        ldr r1, [r3, #20]
         mul r1, r0
-        ldr r0, [r5, #28]
+        ldr r0, [r3, #28]
         mul r1, r0
 
         sub r2, r1
 //4
 
-        ldr r0, [r5, #4]
-        ldr r1, [r5, #12]
+        ldr r0, [r3, #4]
+        ldr r1, [r3, #12]
         mul r1, r0
-        ldr r0, [r5, #32]
+        ldr r0, [r3, #32]
         mul r1, r0
 
         sub r2, r1
 //5
 
-        ldr r0, [r5, #8]
-        ldr r1, [r5, #16]
+        ldr r0, [r3, #8]
+        ldr r1, [r3, #16]
         mul r1, r0
-        ldr r0, [r5, #24]
+        ldr r0, [r3, #24]
         mul r1, r0
 
         sub r2, r1
