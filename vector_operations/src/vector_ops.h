4bee494b010c24043c82290579e5bdbc7f8ffb6f
lakuzne4 <kuznetsov_l@inbox.ru>
2020-10-17 18:39:58 +0300

    Update vector_ops.h
    Добавил фикс. Сделал кодстайл как LLVM. Убрал все push_back, добавил resize, std::swap и избавился от деления на 0 с помощью частичного цикла ищущего первый ненулевой элемент во втором векторе (и потом проверка на одинаковые коэффициенты начинается с него).

--- vector_operations/src/vector_ops.h
+++ vector_operations/src/vector_ops.h
@@ -2,148 +2,176 @@
 #include <vector>
 #include <iostream>
 
+namespace task
+{
 
-namespace task {
-
-using std::vector;
-using std::ostream;
 using std::istream;
+using std::ostream;
+using std::vector;
 
-ostream& operator << (ostream& ostr , const vector<double>& a){
-	for (size_t i = 0; i< a.size(); ++i) {
-		ostr << a[i] << " ";
-	}
-	ostr << std::endl;
-	return ostr;
-}
-
-
-istream& operator >> (istream& input , vector<double>& a){
-	size_t n;
-	input >> n;
-	if (n == 0) {
-		a.clear();
-		return input;
-	}
-	double d;
-	for (size_t i =0; i<n; ++ i) {
-		input >> d;
-		a[i] = d;
-	}
-	return input;
-}
-
-
-vector <double> operator+ (const vector<double>& a, const vector<double>& b){
-    vector<double> c;
+ostream &operator<<(ostream &ostr, const vector<double> &a)
+{
     for (size_t i = 0; i < a.size(); ++i)
-        c.push_back(a[i] + b[i]);
+    {
+        ostr << a[i] << " ";
+    }
+    ostr << std::endl;
+    return ostr;
+}
+
+istream &operator>>(istream &input, vector<double> &a)
+{
+    size_t n;
+    input >> n;
+    if (n == 0)
+    {
+        a.clear();
+        return input;
+    }
+    double d;
+    a.resize(n);
+    for (size_t i = 0; i < a.size(); ++i)
+    {
+        input >> d;
+        a[i] = d;
+    }
+    return input;
+}
+
+vector<double> operator+(const vector<double> &a, const vector<double> &b)
+{
+    vector<double> c(a.size());
+    for (size_t i = 0; i < c.size(); ++i)
+        c[i] = a[i] + b[i];
     return c;
 }
 
-
-vector <double> operator+ (const vector<double>& a){
-    vector<double> c;
-    for (size_t i = 0; i < a.size(); ++i)
-        c.push_back(+a[i]);
+vector<double> operator+(const vector<double> &a)
+{
+    vector<double> c(a.size());
+    for (size_t i = 0; i < c.size(); ++i)
+        c[i] = +a[i];
     return c;
 }
 
-
-vector <double> operator- (const vector<double>& a, const vector<double>& b){
-    vector<double> c;
-    for (size_t i = 0; i < a.size(); ++i)
-        c.push_back(a[i] - b[i]);
+vector<double> operator-(const vector<double> &a, const vector<double> &b)
+{
+    vector<double> c(a.size());
+    for (size_t i = 0; i < c.size(); ++i)
+        c[i] = a[i] - b[i];
     return c;
 }
 
-
-vector <double> operator- (const vector<double>& a){
-    vector<double> c;
+vector<double> operator-(const vector<double> &a)
+{
+    vector<double> c(a.size());
     for (size_t i = 0; i < a.size(); ++i)
-        c.push_back(-a[i]);
+        c[i] = -a[i];
     return c;
 }
 
-
-double operator* (const vector<double>& a, const vector<double>& b){
+double operator*(const vector<double> &a, const vector<double> &b)
+{
     double c = 0;
     for (size_t i = 0; i < a.size(); ++i)
-        c += a[i]*b[i];
-        
+        c += a[i] * b[i];
+
     return c;
 }
 
-
-vector<double> operator% (const vector<double>& a, const vector<double>& b){
-    vector <double> c;
-    c.push_back(a[1]*b[2] - a[2]*b[1]);
-    c.push_back(a[2]*b[0] - a[0]*b[2]);
-    c.push_back(a[0]*b[1] - a[1]*b[0]);
+vector<double> operator%(const vector<double> &a, const vector<double> &b)
+{
+    vector<double> c(3);
+    c[0] = a[1] * b[2] - a[2] * b[1];
+    c[1] = a[2] * b[0] - a[0] * b[2];
+    c[2] = a[0] * b[1] - a[1] * b[0];
     return c;
 }
 
-
-bool operator ||(const vector<double>& a, const vector<double>& b){
-	
-	double mul = a[0]/b[0];
-	for (size_t i = 0; i< a.size(); ++i){
-		if (b[i]!=0){
-			if (a[i] - mul * b[i] > 1e-10 ) return false;
-		}
-		else if (a[i] != 0 ) return false;
-		
-	}
-	return true;		
-}
-
-
-bool operator &&(const vector<double>& a, const vector<double>& b){
-    if (!( a || b)) 
-        return false;
-    else 
+bool operator||(const vector<double> &a, const vector<double> &b)
+{
+    double mul = 0;
+    size_t first_not_zero_ind = 0;
+    for (size_t i = 0; i < a.size(); ++i)
     {
-        for (size_t i = 0; i<a.size(); ++i)
+        if ((b[i] == 0) and (a[i] != 0))
+            return false;
+        else if ((b[i] == 0) and (a[i] == 0))
+        {
+            first_not_zero_ind += 1;
+            continue;
+        }
+        else
+        {
+            mul = a[i] / b[i];
+            break;
+        }
+        first_not_zero_ind = i;
+    }
+    if (first_not_zero_ind == a.size())
+        return true;
+
+    for (size_t i = first_not_zero_ind; i < a.size(); ++i)
+    {
+        if (b[i] != 0)
+        {
+            if (a[i] - mul * b[i] > 1e-10)
+                return false;
+        }
+        else if (a[i] != 0)
+            return false;
+    }
+    return true;
+}
+
+bool operator&&(const vector<double> &a, const vector<double> &b)
+{
+    if (!(a || b))
+        return false;
+    else
+    {
+        for (size_t i = 0; i < a.size(); ++i)
+        {
+            if (b[i] != 0)
             {
-                if (b[i]!=0)
-                {
-                    if (a[i]/b[i] > 0)
-                        return true;
-                    else return false;
-                }
+                if (a[i] / b[i] > 0)
+                    return true;
+                else
+                    return false;
             }
+        }
         return false;
     }
 }
 
-
-vector<double> reverse (vector<double>& a){
+vector<double> reverse(vector<double> &a)
+{
     double tmp;
-    for (size_t i = 0; i < a.size()/2 ;  ++i )
-        {
-			tmp = a[i];
-			a[i] = a[a.size() - 1 - i];
-			a[a.size() - 1 - i] = tmp;
-        }
+    for (size_t i = 0; i < a.size() / 2; ++i)
+    {
+        std::swap(a[i], a[a.size() - 1 - i]);
+    }
     return a;
 }
 
-
-vector<int> operator | (const vector<int>& a, const vector<int>& b){
-    vector<int> c;
-    for (size_t i = 0; i < a.size() ; ++i ){
-        c.push_back( a[i] | b[i] );
+vector<int> operator|(const vector<int> &a, const vector<int> &b)
+{
+    vector<int> c(a.size());
+    for (size_t i = 0; i < a.size(); ++i)
+    {
+        c[i] = a[i] | b[i];
     }
     return c;
 }
 
-
-vector<int> operator & (const vector<int>& a, const vector<int>& b){
-    vector<int> c;
-    for (size_t i = 0; i < a.size() ; ++i ){
-        c.push_back( a[i] & b[i] );
+vector<int> operator&(const vector<int> &a, const vector<int> &b)
+{
+    vector<int> c(a.size());
+    for (size_t i = 0; i < a.size(); ++i)
+    {
+        c[i] = a[i] & b[i];
     }
     return c;
 }
 
-}  // namespace task
+} // namespace task
